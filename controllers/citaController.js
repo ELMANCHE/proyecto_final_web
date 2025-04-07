d553b158a96bee95c3ac17f65506a42ee181cdea
ELMANCHE <110752573+ELMANCHE@users.noreply.github.com>
2025-04-06 19:37:47 -0500

    control de citas
    control de citas

---
+++ controllers/citaController.js
@@ -0,0 +1,18 @@
+// controllers/citaController.js
+const Cita = require('../models/Cita');
+
+// Crear cita
+exports.createCita = async (req, res) => {
+  try {
+    const cita = await Cita.create(req.body);
+    res.status(201).json(cita);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Obtener todas las citas
+exports.getCitas = async (req, res) => {
+  const citas = await Cita.find().populate('paciente');
+  res.json(citas);
+};
